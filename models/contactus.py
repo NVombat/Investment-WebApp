28faf1dbbcb8f7a82bee3f17f12be1fdb8b182b7
NVombat <nvombatkere@gmail.com>
2021-04-05 21:38:35 +0530

    feat(*): init backend

--- models/contactus.py
+++ models/contactus.py
@@ -1,15 +1,15 @@
 import sqlite3 as s
 
 def create_tbl(path : str):
-    conn = s.connect("app.db")
+    conn = s.connect(path)
     cur = conn.cursor()
 
     tbl = 'CREATE TABLE IF NOT EXISTS contact_us(Email TEXT, Message TEXT)'
     cur.execute(tbl)
     conn.commit()
 
-def insert(email : str, message : str):
-    conn = s.connect("app.db")
+def insert(email : str, message : str, path : str):
+    conn = s.connect(path)
     cur = conn.cursor()
 
     insrt = f"INSERT INTO contact_us VALUES('{email}','{message}')"
--- models/stock.py
+++ models/stock.py
@@ -1,25 +1,41 @@
 import sqlite3 as s
 
-def make_tbl():
-    conn = s.connect("app.db")
+def make_tbl(path : str):
+    conn = s.connect(path)
     cur = conn.cursor()
 
-    tbl = "CREATE TABLE IF NOT EXISTS stock(Date Date, Stock_Symbol Text, Price real, Quantity int)"
+    tbl = "CREATE TABLE IF NOT EXISTS stock(Date Date, Stock_Symbol Text, Price real, Quantity int, Email Text)"
     cur.execute(tbl)
     conn.commit()
 
-def buy(tablename : str, data : tuple):
-    conn = s.connect("app.db")
+def buy(tablename : str, data : tuple, path : str):
+    conn = s.connect(path)
     cur = conn.cursor()
 
     b = f"INSERT INTO {tablename} VALUES {data}"
     cur.execute(b)
     conn.commit()
 
-def sell(tablename : str, sym : str):
-    conn = s.connect("app.db")
+def sell(tablename : str, sym : str, path : str):
+    conn = s.connect(path)
     cur = conn.cursor()
 
-    s = f"DELETE FROM {tablename} WHERE Stock_Symbol={sym}"
-    cur.execute(s)
+    sel = f"DELETE FROM {tablename} WHERE Stock_Symbol={sym}"
+    cur.execute(sel)
     conn.commit()
+
+def query(email : str, path : str):
+    conn = s.connect(path)
+    cur = conn.cursor()
+
+    que = f"SELECT * FROM stock WHERE Email ='{email}'"
+    cur.execute(que)
+    res = cur.fetchall()
+    return res
+
+if __name__ == "__main__":
+    path = "../app.db"
+    make_tbl("../app.db")
+    buy("stock", ("19-9-2000", "GOOGL", 354.9, 14, "ronaldo72emiway@gmail.com"), path)
+    buy("stock", ("23-7-2002", "AAPL", 162.4, 10, "ronaldo72emiway@gmail.com"), path)
+
