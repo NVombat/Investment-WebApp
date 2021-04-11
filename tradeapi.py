36aac52ecaa46e77b9b4236b5c5d0a825172b612
Aradhya-Tripathi <at8029@srmist.edu.in>
2021-04-11 17:56:51 +0530

    (buy api): added api route for buying

--- tradeapi.py
+++ tradeapi.py
@@ -1,25 +1,12 @@
-#Import Libraries and functions
-from fastapi import FastAPI 
+# Import Libraries and functions
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from pydantic import BaseModel
 from models import stock
-
-
-import sqlite3
-
 
 app = FastAPI()
 
 @app.get('/buy')
-async def buy(user : str):
-    
-    response = RedirectResponse(url='http://localhost:8000/trade')
-    
-    return response
-
-@app.get('/sell')
-async def sell(user : str):
-    
-    response = RedirectResponse(url='http://localhost:8000/trade')
-    
-    return response
+def buy_stocks(date:str, stock_symbol: str, price: float, quantity: int, email: str,
+               tablename: str='stock',path: str='app.db'):
+    data = (date, stock_symbol, price, quantity, email)
+    stock.buy(tablename, data, path=path)
