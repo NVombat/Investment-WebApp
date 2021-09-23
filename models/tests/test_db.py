b3c76f57ab9c425df44b4b16dedabf84d1a93a24
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-09-23 13:10:28 +0530

    refactor: refactored test directories fixes #92

--- models/tests/test_db.py
+++ models/tests/test_db.py
@@ -6,20 +6,30 @@ import models.contactus as ct
 import models.stock as st
 import models.users as us
 
-from test_data import Base
+from .test_data import Base
+
 data = Base()
 
-class TestDatabase(unittest.TestCase):
 
+class TestDatabase(unittest.TestCase):
     def test_path(self):
-        self.assertEqual("True", str(os.path.exists(data.contact_us_data['path'])))
+        self.assertEqual("True", str(os.path.exists(data.contact_us_data["path"])))
         self.assertEqual("False", str(os.path.exists("apq.db")))
 
     def test_stock_buy(self):
-        self.assertIsNone(st.buy(data.test_data["tablename"],
-                        (data.test_data["date"], data.test_data["stock_symbol"],
-                        data.test_data["price"], data.test_data["quantity"],
-                        data.test_data["email"]), data.test_data['path']))
+        self.assertIsNone(
+            st.buy(
+                data.test_data["tablename"],
+                (
+                    data.test_data["date"],
+                    data.test_data["stock_symbol"],
+                    data.test_data["price"],
+                    data.test_data["quantity"],
+                    data.test_data["email"],
+                ),
+                data.test_data["path"],
+            )
+        )
 
         # ADD CHECK TO REFUSE ANYTHING BUT INT
         # self.assertIsNone(st.buy(data.test_data["tablename"],
@@ -29,6 +39,7 @@ class TestDatabase(unittest.TestCase):
 
     def test_users_(self):
         pass
+
 
 if __name__ == "__main__":
     unittest.main()
