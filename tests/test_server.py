9010ac726b7ddaa044ad1c129b6906539be712e2
NVombat <nvombatkere@gmail.com>
2021-09-28 23:08:43 +0530

    Server tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,11 +2,10 @@ import unittest
 import requests
 import warnings
 import pytest
+import os
 
-from back import (
-    get_current_stock_price,
-)
-
+from back import get_current_stock_price, Currency_Conversion
+from dotenv import load_dotenv
 from . import Base
 
 data = Base()
@@ -18,6 +17,18 @@ class TestServer(unittest.TestCase):
             action="ignore", message="unclosed", category=ResourceWarning
         )
         assert type(get_current_stock_price(data.stock_data["stock_symbol"])) == float
+
+    def test_currency_conversion(self):
+        load_dotenv()
+        url = str.__add__(
+            "http://data.fixer.io/api/latest?access_key=",
+            os.getenv("CURRENCY_ACCESS_KEY"),
+        )
+        c = Currency_Conversion(url)
+        from_country = "USD"
+        to_country = "INR"
+        amount = 100
+        assert type(c.convert(from_country, to_country, amount)) == float
 
 
 if __name__ == "__main__":
