8d527129ca0b292beb5c39bc02b66c8090405d9d
NVombat <nvombatkere@gmail.com>
2022-11-18 03:42:57 +0530

    fixed Email API

--- sendmail.py
+++ sendmail.py
@@ -1,11 +1,10 @@
 # Imports
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from models.users import add_code
-import smtplib, ssl
-import sqlite3
-import random
 import os
+import random
+import smtplib
+import sqlite3
+
+from models.users import add_code
 
 
 def send_mail(path: str, email: str) -> None:
@@ -18,14 +17,14 @@ def send_mail(path: str, email: str) -> None:
     Returns:
         None
     """
-    conn = sqlite3.connect(path)
-    cur = conn.cursor()
+    MAILGUN_EMAIL = os.getenv("MAILGUN_EMAIL")
+    MAILGUN_PWD = os.getenv("MAILGUN_PWD")
 
-    backemail_add = os.environ.get("backend_mail")
-    backemail_pwd = os.environ.get("backend_pwd")
-
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login(backemail_add, backemail_pwd)
+    try:
+        server = smtplib.SMTP("smtp.mailgun.org", 587)
+        server.login(MAILGUN_EMAIL, MAILGUN_PWD)
+    except:
+        print("Error Connecting To Mail Server")
 
     key = random.randint(1000, 9999)
     add_code(path, key, email)
@@ -36,7 +35,11 @@ def send_mail(path: str, email: str) -> None:
     body = f'Dear User\nPlease Click on the Link Below to Reset your "Code"Vid19 Password for your {email} account.\n\nThis is your 4 Digit Verification Code: {key} \n\nLink: {url} \n\nIf you didnt ask to reset your password please IGNORE this email!\n\nThank you\nWarm Regards\nTeam "Code"Vid19'
     msg = f"Subject: {subject}\n\n{body}"
 
-    server.sendmail(backemail_add, email, msg)
+    try:
+        server.sendmail(MAILGUN_EMAIL, email, msg)
+    except:
+        print("Error Sending Mail")
+
     server.quit()
 
 
@@ -56,20 +59,24 @@ def send_buy(path: str, data: tuple) -> None:
     Returns:
         None
     """
-    conn = sqlite3.connect(path)
-    cur = conn.cursor()
+    MAILGUN_EMAIL = os.getenv("MAILGUN_EMAIL")
+    MAILGUN_PWD = os.getenv("MAILGUN_PWD")
 
-    backemail_add = os.environ.get("backend_mail")
-    backemail_pwd = os.environ.get("backend_pwd")
-
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login(backemail_add, backemail_pwd)
+    try:
+        server = smtplib.SMTP("smtp.mailgun.org", 587)
+        server.login(MAILGUN_EMAIL, MAILGUN_PWD)
+    except:
+        print("Error Connecting To Mail Server")
 
     subject = "Stock Transaction Receipt: IMP!"
     body = f'Dear User\nHere is your transaction receipt for your {data[4]} account.\n\nYou purchased {data[2]} units of the {data[0]} stock on {data[5]} at a rate of $ {data[1]} per stock unit.\n\nYour total expenditure was $ {data[3]}. Thank you for using "Code"vid19 Solutions.\n\nIf you did not make or authorize this transaction PLEASE CONTACT US IMMEDIATELY!\n\nThank you\nWarm Regards\nTeam "Code"Vid19'
     msg = f"Subject: {subject}\n\n{body}"
 
-    server.sendmail(backemail_add, data[4], msg)
+    try:
+        server.sendmail(MAILGUN_EMAIL, data[4], msg)
+    except:
+        print("Error Sending Mail")
+
     server.quit()
 
 
@@ -89,18 +96,22 @@ def send_sell(path: str, data: tuple) -> None:
     Returns:
         None
     """
-    conn = sqlite3.connect(path)
-    cur = conn.cursor()
+    MAILGUN_EMAIL = os.getenv("MAILGUN_EMAIL")
+    MAILGUN_PWD = os.getenv("MAILGUN_PWD")
 
-    backemail_add = os.environ.get("backend_mail")
-    backemail_pwd = os.environ.get("backend_pwd")
-
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login(backemail_add, backemail_pwd)
+    try:
+        server = smtplib.SMTP("smtp.mailgun.org", 587)
+        server.login(MAILGUN_EMAIL, MAILGUN_PWD)
+    except:
+        print("Error Connecting To Mail Server")
 
     subject = "Stock Transaction Receipt: IMP!"
     body = f'Dear User\nHere is your transaction receipt for your {data[4]} account.\n\nYou sold {data[2]} units of the {data[0]} stock on {data[5]} at a rate of $ {data[1]} per stock unit.\n\nYour total earning was $ {data[3]}. Thank you for using "Code"vid19 Solutions.\n\nIf you did not make or authorize this transaction PLEASE CONTACT US IMMEDIATELY!\n\nThank you\nWarm Regards\nTeam "Code"Vid19'
     msg = f"Subject: {subject}\n\n{body}"
 
-    server.sendmail(backemail_add, data[4], msg)
+    try:
+        server.sendmail(MAILGUN_EMAIL, data[4], msg)
+    except:
+        print("Error Sending Mail")
+
     server.quit()
